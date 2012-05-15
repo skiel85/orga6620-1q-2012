064742f6d10ef4e39a96590c1eb93d2aa018f572
juanmg0511@gmail.com <juanmg0511@gmail.com@aba71715-cc5d-e762-c2d3-ca702094212e>
2012-05-15 13:24:11 +0000

    agrego memcpy a mymemory.h y .S

--- trunk/tp1/version MIPS32/mymemory.h
+++ trunk/tp1/version MIPS32/mymemory.h
@@ -9,5 +9,6 @@
 #define __MYMEMORY_H__
 
 extern void *myrealloc(void *ptr, size_t old_size, size_t size);
+extern void *mymemcpy (void * destination, const void * source, size_t num); 
 
 #endif /* __MYMEMORY_H__ */
