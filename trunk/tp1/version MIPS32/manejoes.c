799a7167ca2469885c1c24b333de44a0e18e0d01
elosi@mail.com <elosi@mail.com@aba71715-cc5d-e762-c2d3-ca702094212e>
2012-05-15 03:29:27 +0000

    Agregado el myrealloc

--- trunk/tp1/version MIPS32/manejoes.c
+++ trunk/tp1/version MIPS32/manejoes.c
@@ -5,7 +5,6 @@
 
 #include "def_errores.h"
 #include "manejoes.h"
-#include "mymalloc.h"
 
 void * mymemcpy (void * destination, const void * source, size_t num) {
 
@@ -20,38 +19,6 @@ void * mymemcpy (void * destination, const void * source, size_t num) {
 	return(resultado);
 }
 
-void *myrealloc(void *ptr, size_t old_size, size_t size) {
-
-	int minsize;
-	void *newptr;
-	
-	// Allocate new block, returning NULL if not possible.	
-	newptr = mymalloc (size);
-	if (newptr == NULL)
-		return NULL;
-
-	// Don't copy/free original block if it was NULL.
-	if (ptr != NULL) {
-		// Get size to copy - mm_getsize must give you the size of the current block.
-		// But, if new size is smaller, only copy that much. Many implementations
-		// actually reserve the 16 bytes in front of the memory to store this info, e.g.,
-		// +--------+--------------------------------+
-		// | Header | Your data                      |
-		// +--------+--------------------------------+
-		//           ^
-		//           +--- this is your pointer.
-		// <- This is the memory actually allocated ->
-	
-		//minsize = mm_getsize (ptr);
-		minsize = old_size;
-	
-		// Copy the memory, free the old block and return the new block.
-		mymemcpy (newptr, ptr, minsize);
-		myfree (ptr);
-	}
-
-	return newptr;
-}
 
 /* FIN DE FUNCIONES DE E/S (manejoes.c) */
 
---
+++ trunk/tp1/version MIPS32/mymemory.h
@@ -0,0 +1,13 @@
+/*
+ * mymemory.h
+ *
+ *  Created on: 14/05/2012
+ *      Author: osiris
+ */
+
+#ifndef __MYMEMORY_H__
+#define __MYMEMORY_H__
+
+extern void *myrealloc(void *ptr, size_t old_size, size_t size);
+
+#endif /* __MYMEMORY_H__ */
